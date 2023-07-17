08e9faf132d7092abc56258ef1da393ac28834a5
Eshika rawat <eshikarawat02@gmail.com>
2023-07-17 19:45:47 +0530

    created datatable.tsx

---
+++ app/DataTable.tsx
@@ -0,0 +1,76 @@
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface DataTableProps {
+  headers: string[];
+  rows: (string | JSX.Element)[][];
+  caption?: string;
+  sortable?: boolean;
+  pagination?: boolean;
+}
+
+const DataTable: React.FC<DataTableProps> = ({ headers, rows, caption, sortable, pagination }) => {
+  const [sortedColumn, setSortedColumn] = useState<string | null>(null);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+
+  const handleSort = (column: string) => {
+    if (!sortable) return;
+
+    if (column === sortedColumn) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortedColumn(column);
+      setSortDirection("asc");
+    }
+  };
+
+  const sortedRows = [...rows].sort((a, b) => {
+    if (!sortedColumn) return 0;
+  
+    const columnIndex = headers.indexOf(sortedColumn);
+    if (columnIndex === -1) return 0;
+  
+    const aValue = a[columnIndex];
+    const bValue = b[columnIndex];
+  
+    if (typeof aValue === "string" && typeof bValue === "string") {
+      if (sortDirection === "asc") return aValue.localeCompare(bValue);
+      else return bValue.localeCompare(aValue);
+    }
+  
+    return 0;
+  });
+  
+  return (
+    <Box>
+      {caption && <Text fontWeight="bold">{caption}</Text>}
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            {headers.map((header, index) => (
+              <Th
+                key={index}
+                onClick={() => handleSort(header)}
+                cursor={sortable ? "pointer" : "default"}
+                _hover={sortable ? { textDecoration: "underline" } : {}}
+              >
+                {header}
+              </Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {sortedRows.map((row, rowIndex) => (
+            <Tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <Td key={cellIndex}>{cell}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+};
+
+export default DataTable;
